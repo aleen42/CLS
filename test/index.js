324211710d91b7b487cd0a424c578dfbb80818b5
aleen42 <aleen42@vip.qq.com>
2016-05-11 18:16:49 +0800

    upload files

---
+++ test/index.js
@@ -0,0 +1,35 @@
+/***********************************************************************
+ *                                                                   _
+ *       _____  _                           ____  _                 |_|
+ *      |  _  |/ \   ____  ____ __ ___     / ___\/ \   __   _  ____  _
+ *      | |_| || |  / __ \/ __ \\ '_  \ _ / /    | |___\ \ | |/ __ \| |
+ *      |  _  || |__. ___/. ___/| | | ||_|\ \___ |  _  | |_| |. ___/| |
+ *      |_/ \_|\___/\____|\____||_| |_|    \____/|_| |_|_____|\____||_| 
+ *                                                                      
+ *      ================================================================
+ *                 More than a coder, More than a designer              
+ *      ================================================================
+ *
+ *
+ *      - Document: index.js
+ *      - Author: aleen42
+ *      - Description: this is the entrace of some tests
+ *      - Create Time: May 11st, 2016
+ *      - Update Time: May 11st, 2016
+ *
+ *
+ **********************************************************************/
+
+const cls = require('../lib/index');
+
+/**
+ * [should: test framework module]
+ * @type {Object}
+ */
+const should = require('chai').should();
+
+describe('tests', function () {
+	it('test case', function () {
+    	cls.test().should.equal('\n\n\nUseAge: cls -u <user_name> [options]\n                                                                  _\n      _____  _                           ____  _                 |_|\n     |  _  |/ \\   ____  ____ __ ___     / ___\\/ \\   __   _  ____  _\n     | |_| || |  / __ \\/ __ \\\\ \'_  \\ _ / /    | |___\\ \\ | |/ __ \\| |\n     |  _  || |__. ___/. ___/| | | ||_|\\ \\___ |  _  | |_| |. ___/| |\n     |_/ \\_|\\___/\\____|\\____||_| |_|    \\____/|_| |_|_____|\\____||_| \n                                                                     \n     ================================================================\n                More than a coder, More than a designer              \n     ================================================================\n\n\n\n  -u, --user=ARG         \tGitHub\'s user_name\n      --filter[=filter]  \tSatistic filter: \n\t(ex.)\n\t--filter=all \t\tfor all repositories (default), \n\t--filter=original \tfor original repositories\n      --color[=corlor]   \tSatistic filter: \n\t(ex.)\n\t--color=random \t\tfor badges with random colors (default), \n\t--color=blue \t\tfor badges with blue themes, \n\t--color=red \t\tfor badges with red themes\n  -a                     \tShow accurate number\n      --badges-lists     \tShow a list of badges for markdown\n  -h, --help             \tTutorial for this command\n\n\n\n\n');
+    });
+});
